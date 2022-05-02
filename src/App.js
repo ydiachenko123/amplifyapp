d5264a26ae696dcd661e257ac0c05bbd4a9bc700
Єгор Дяченко <ydiachenko@web100-sumy.local>
2022-05-02 16:12:36 +0300

    buncerclub

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, BuncerClub!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.pornhub.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit our website
         </a>
       </header>
     </div>
